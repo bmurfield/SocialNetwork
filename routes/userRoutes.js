41a2bcb19b0aad4c9fa7fd65bb05a20f6391b26e
Marv <bmurfield@gmail.com>
2024-11-21 14:22:19 -0600

    added api routes, need to test more

---
+++ routes/userRoutes.js
@@ -0,0 +1,107 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+const Thought = require('../models/Thought');
+
+// GET all users
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET a single user by its _id and populated thought and friend data
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).populate('thoughts').populate('friends');
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// POST a new user
+router.post('/', async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// PUT to update a user by its _id
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!updatedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.json(updatedUser);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// DELETE a user by its _id and bonus: remove associated thoughts
+router.delete('/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Optionally delete associated thoughts
+    await Thought.deleteMany({ _id: { $in: user.thoughts } });
+
+    res.json({ message: 'User and associated thoughts deleted' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// POST to add a friend
+router.post('/:userId/friends/:friendId', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    const friend = await User.findById(req.params.friendId);
+    if (!user || !friend) {
+      return res.status(404).json({ message: 'User or friend not found' });
+    }
+
+    if (!user.friends.includes(friend._id)) {
+      user.friends.push(friend._id);
+      await user.save();
+    }
+
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE to remove a friend
+router.delete('/:userId/friends/:friendId', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    const friend = await User.findById(req.params.friendId);
+    if (!user || !friend) {
+      return res.status(404).json({ message: 'User or friend not found' });
+    }
+
+    user.friends = user.friends.filter(f => f.toString() !== friend._id.toString());
+    await user.save();
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
